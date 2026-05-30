53975569a41ae71c8d92dd30399df2589293ef30
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-30 19:32:00 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class clusterRonnectWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = clusterRonnectWorker()
+    print("Worker engine initialized.")
